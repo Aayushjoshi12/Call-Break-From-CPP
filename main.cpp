30bbc9a1471f31b6c319e7e5cde11dd859d0d0a2
Aayushjoshi12 <100678671+Aayushjoshi12@users.noreply.github.com>
2026-05-20 21:33:17 +0545

    First commit

--- main.cpp
+++ main.cpp
@@ -2,14 +2,25 @@
 #include<iostream>
 
 using namespace std;
-int screenwidth = 900;screenheight = 900;
+float screenwidth = 900,screenheight = 900;
+
 
 int main(){
     InitWindow(screenwidth,screenheight,"Cpp");
+    Texture2D img = LoadTexture("Image files/BackGround.png");
     SetTargetFPS(100);
     while(WindowShouldClose() == false){
         BeginDrawing();
-        ClearBackground(Red); 
+        ClearBackground(BLACK);
+            DrawTexturePro(
+            img,
+            (Rectangle){ 0, 0, (float)img.width, (float)img.height },
+            (Rectangle){ 0, 0, (float)screenwidth, (float)screenheight },
+            (Vector2){ 0,0},
+            0.0f,
+            WHITE
+        );
         EndDrawing();
-}
+    }
+    UnloadTexture(img);
 }
